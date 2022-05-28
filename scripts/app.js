0f057ea3d5f002bd101edd2af1988d3a5390a33a
Lester Yiu <lester.ws.yiu@gmail.com>
2022-05-28 05:00:20 -0400

    reorganized the namespace to ensure query selects are not in global scope

--- scripts/app.js
+++ scripts/app.js
@@ -1,116 +1,102 @@
 const cookingApp = {};
 
-cookingApp.apiKey = `b816c6f070174a9596e8c0889839e0da`;
-
+cookingApp.apiKey = `8e7633fbea684358a5fde199da86bb01`;
 cookingApp.endpoint = `https://api.spoonacular.com/recipes/complexSearch`;
-
 cookingApp.recipeEndPoint = `https://api.spoonacular.com/recipes/analyze`;
 
-const formEl = document.querySelector('form');
-
-const dairyChoice = document.querySelector(`input[id="dairy"]`); //checked
-
-const glutenChoice = document.querySelector(`input[id="gluten"]`);
-
-const vegetarianChoice = document.querySelector(`input[id="vegetarian-diet"]`);
-
-const veganChoice = document.querySelector(`input[id="vegan-diet"]`);
-
-const ulEl = document.querySelector('ul.column');
-
-const openMenuEl = document.querySelector('.slideout-button');
-
-const exitMenuEl = document.querySelector('.close-menu-button');
-
-const slideMenu = document.querySelector('.slidemenu-nav');
-
-const nothingYetEl = document.querySelector('.nothing-yet');
-
-const noResultsEl = document.querySelector('.no-results');
+cookingApp.formEl = document.querySelector('form');
+cookingApp.dairyChoiceEl = document.querySelector(`input[id="dairy"]`)
+cookingApp.glutenChoiceEl = document.querySelector(`input[id="gluten"]`);
+cookingApp.vegetarianChoiceEl = document.querySelector(`input[id="vegetarian-diet"]`);
+cookingApp.veganChoiceEl = document.querySelector(`input[id='vegan-diet']`);
+cookingApp.cuisineChoiceEl = document.querySelector(`select[name='type-of-cuisine']`);
+cookingApp.ingredientChoiceEl = document.querySelector(`input[type='text']`);
+cookingApp.ulEl = document.querySelector('ul.column');
+cookingApp.openMenuEl = document.querySelector('.slideout-button');
+cookingApp.exitMenuEl = document.querySelector('.close-menu-button');
+cookingApp.slideMenuEl = document.querySelector('.slidemenu-nav');
+cookingApp.nothingYetEl = document.querySelector('.nothing-yet');
+cookingApp.noResultsEl = document.querySelector('.no-results');
 
 cookingApp.openMenu = function () {
-    openMenuEl.addEventListener('click', function() {
-        slideMenu.classList.toggle("slidemenu-nav-closed");
-        slideMenu.classList.toggle("slidemenu-nav-open");
+    cookingApp.openMenuEl.addEventListener('click', function() {
+        cookingApp.slideMenuEl.classList.toggle("slidemenu-nav-closed");
+        cookingApp.slideMenuEl.classList.toggle("slidemenu-nav-open");
     });
 }
 
 cookingApp.closeMenu = function () {
-    exitMenuEl.addEventListener('click', function() {
-        slideMenu.classList.toggle("slidemenu-nav-closed");
-        slideMenu.classList.toggle("slidemenu-nav-open");
+    cookingApp.exitMenuEl.addEventListener('click', function() {
+        cookingApp.slideMenuEl.classList.toggle("slidemenu-nav-closed");
+        cookingApp.slideMenuEl.classList.toggle("slidemenu-nav-open");
     })
 }
 
-cookingApp.slideMenu = function () {
+cookingApp.getSlideMenu = function () {
 
     cookingApp.openMenu();
 
     cookingApp.closeMenu();
 
-    slideMenu.addEventListener('click', function(e) {
+    cookingApp.slideMenuEl.addEventListener('click', function(e) {
         if (e.target.localName === "a") {
-            slideMenu.classList.toggle("slidemenu-nav-closed");
-            slideMenu.classList.toggle("slidemenu-nav-open");
+            cookingApp.slideMenuEl.classList.toggle("slidemenu-nav-closed");
+            cookingApp.slideMenuEl.classList.toggle("slidemenu-nav-open");
         }
     })
 }
 
 cookingApp.placeEventListeners = function() {
-    dairyChoice.addEventListener('click', () => {
-        if (dairyChoice.checked === true) {
-            cookingApp.dairyValue = dairyChoice.value;
+    cookingApp.dairyChoiceEl.addEventListener('click', () => {
+        if (cookingApp.dairyChoiceEl.checked === true) {
+            cookingApp.dairyValue = cookingApp.dairyChoiceEl.value;
         } else {
             cookingApp.dairyValue = '';
         }
     });
 
-    glutenChoice.addEventListener('click', () => {
-        if (glutenChoice.checked === true) {
-            cookingApp.glutenValue = glutenChoice.value;
+    cookingApp.glutenChoiceEl.addEventListener('click', () => {
+        if (cookingApp.glutenChoiceEl.checked === true) {
+            cookingApp.glutenValue = cookingApp.glutenChoiceEl.value;
         } else {
             cookingApp.glutenValue = '';
         }
     });
 
-    vegetarianChoice.addEventListener('click', () => {
-        if (vegetarianChoice.checked === true) {
-            cookingApp.dietValue = vegetarianChoice.value;
-            const veganChoice = document.querySelector(`input[id='vegan-diet']`);
-            veganChoice.checked = false;
+    cookingApp.vegetarianChoiceEl.addEventListener('click', () => {
+        if (cookingApp.veganChoiceEl.checked === true) {
+            cookingApp.veganChoiceEl.checked === false;
+            cookingApp.dietValue = '';
+            cookingApp.dietValue = cookingApp.vegetarianChoiceEl.value;
+            cookingApp.veganChoiceEl.checked = false;
+        } else if (cookingApp.vegetarianChoiceEl.value && cookingApp.vegetarianChoiceEl.checked === false) {
+            cookingApp.dietValue = '';
         } else {
             cookingApp.dietValue = '';
+            cookingApp.dietValue = cookingApp.vegetarianChoiceEl.value;
         }
     });
 
-    veganChoice.addEventListener('click', () => {
-        if (veganChoice.checked === true) {
-            cookingApp.dietValue = veganChoice.value;
-            const vegetarianChoice = document.querySelector(`input[id='vegetarian-diet']`);
-            vegetarianChoice.checked = false;
+    cookingApp.veganChoiceEl.addEventListener('click', () => {
+        if (cookingApp.vegetarianChoiceEl.checked === true) {
+            cookingApp.vegetarianChoiceEl.checked === false;
+            cookingApp.dietValue = '';
+            cookingApp.dietValue = cookingApp.veganChoiceEl.value;
+            cookingApp.vegetarianChoiceEl.checked = false;
+        } else if (cookingApp.veganChoiceEl.value && cookingApp.veganChoiceEl.checked === false) {
+            cookingApp.dietValue = '';
         } else {
             cookingApp.dietValue = '';
-        } 
+            cookingApp.dietValue = cookingApp.veganChoiceEl.value;
+        }
     });
 }
 
 cookingApp.submissionForm = function() {
 
-    formEl.addEventListener('submit', (e) => {
+    cookingApp.formEl.addEventListener('submit', (e) => {
         e.preventDefault();
-
-        nothingYetEl.remove();
-
-        cookingApp.dairyChoice = document.querySelector(`input[id="dairy"]`)
-
-        cookingApp.vegetarianChoice = document.querySelector(`input[id="vegetarian-diet"]`);
-
-        cookingApp.veganChoice = document.querySelector(`input[id='vegan-diet']`);
-
-        cookingApp.cuisineChoice = document.querySelector(`select[name='type-of-cuisine']`); // .selected
-
-        cookingApp.ingredientChoice = document.querySelector(`input[type='text']`); // value
-
+        cookingApp.nothingYetEl.remove();
         cookingApp.getInfo();
     });
 
@@ -119,15 +105,15 @@ cookingApp.submissionForm = function() {
 cookingApp.getInfo = () => {
 
     const spoonUrl = new URL (cookingApp.endpoint);
-    
+
     spoonUrl.search = new URLSearchParams({
         apiKey: cookingApp.apiKey,
         addRecipeInformation: true,
         instructionsRequired: true, //This is in analyzed instructions
-        query: cookingApp.ingredientChoice.value,
+        query: cookingApp.ingredientChoiceEl.value,
         intolerances: `${cookingApp.dairyValue}, ${cookingApp.glutenValue}`,
         diet: cookingApp.dietValue,
-        cuisine: cookingApp.cuisineChoice.value
+        cuisine: cookingApp.cuisineChoiceEl.value
     });
 
     fetch(spoonUrl)
@@ -136,14 +122,16 @@ cookingApp.getInfo = () => {
         })
             .then ( (jsonData) => {
                 if (jsonData.results.length > 0) {
-                    noResultsEl.classList.add('display-none');
-                    ulEl.innerHTML = '';
+                    cookingApp.noResultsEl.classList.add('display-none');
+                    cookingApp.ulEl.innerHTML = '';
                     jsonData.results.forEach( (item) => {
                         cookingApp.appendItems(item);
                     })                    
                 } else {
-                    ulEl.innerHTML = '';
-                    noResultsEl.classList.toggle('display-none');
+                    cookingApp.ulEl.innerHTML = '';
+                    if (cookingApp.noResultsEl.className === 'no-results display-none') {
+                        cookingApp.noResultsEl.classList.toggle('display-none');
+                    }
                 }
 
             });
@@ -152,57 +140,42 @@ cookingApp.getInfo = () => {
 cookingApp.appendItems = (argument) => {
 
     const newAnchorEl = document.createElement('a');
-
     newAnchorEl.href = argument.sourceUrl;
-
     newAnchorEl.target = '_blank';
-
     newAnchorEl.rel = 'noopener';
 
     const newLiEl = document.createElement('li');
-
     newLiEl.classList.add('box');
-
     newAnchorEl.appendChild(newLiEl);
 
     const newImageContEl = document.createElement('div');
-
     newImageContEl.classList.add('img-wrapper');
-
     newLiEl.appendChild(newImageContEl);
 
     const newTextContEl = document.createElement('div');
-
     newTextContEl.classList.add('text-container');
-
     newLiEl.appendChild(newTextContEl);
 
     const newImageEl = document.createElement('img');
-
     newImageEl.src = `${argument.image}`;
-
     newImageEl.alt = `${argument.title}`;
-
     newImageContEl.appendChild(newImageEl);
 
     const newHeadingEl = document.createElement('h3');
-
     newHeadingEl.innerText = `${argument.title}`;
-
     newTextContEl.appendChild(newHeadingEl);
-
     cookingApp.displayItems(newAnchorEl);
 }
 
 cookingApp.displayItems = (item) => {
 
-    ulEl.appendChild(item);
+    cookingApp.ulEl.appendChild(item);
 }
 
 cookingApp.init = () => {
     cookingApp.placeEventListeners();
     cookingApp.submissionForm();
-    cookingApp.slideMenu();
+    cookingApp.getSlideMenu();
 }
 
 cookingApp.init();
